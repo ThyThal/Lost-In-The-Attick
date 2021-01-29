b17715adb1e5eeb31a940123d141e4497a6b6381
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-01-29 14:55:31 -0300

    SpawnObjects

---
+++ GGJ-2021-GAME/Assets/_Scripts/SpawnObject.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class SpawnObject : MonoBehaviour
+{
+    [SerializeField] private GameObject prefab = null;
+    [SerializeField] private Transform[] spawnpoints = null;
+
+    private void Start()
+    {
+        if (prefab != null) Spawn(prefab);
+    }
+
+    public void Spawn(GameObject go)
+    {
+        var length = spawnpoints.Length;
+        var randomPoint = length != 0 ? spawnpoints[Random.Range(0, length - 1)] : null;
+
+        if (randomPoint != null) go.transform.position = randomPoint.position;
+    }
+}
