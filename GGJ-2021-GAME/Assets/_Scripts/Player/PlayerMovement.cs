c2556c47397f6325dc30e9236991d5a5aebffd10
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-01-29 19:51:53 -0300

    Aminamciones con el Movimiento del PJ

--- GGJ-2021-GAME/Assets/_Scripts/Player/PlayerMovement.cs
+++ GGJ-2021-GAME/Assets/_Scripts/Player/PlayerMovement.cs
@@ -4,19 +4,23 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float velocity = 10f;
     private Rigidbody2D _rigidbody = null;
+    private Animator _animator = null;
 
     private void Awake()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _animator = GetComponentInChildren<Animator>();
     }
 
     private void Update()
-    {        
+    {
         var movX = Input.GetAxis("Horizontal");
         var movY = Input.GetAxis("Vertical");
 
         var moveDirection = new Vector2(movX, movY).normalized;
         Move(moveDirection);
+        _animator.SetFloat("MovementX", movX);
+        _animator.SetFloat("MovementY", movY);
     }
 
     private void Move(Vector2 direction)
