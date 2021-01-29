7744647b4517dd2e23c650297c551014d242660f
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-01-29 18:12:38 -0300

    Fear 1st Approach

---
+++ GGJ-2021-GAME/Assets/_Scripts/Player/FearMeter.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+public class FearMeter : MonoBehaviour
+{
+    [SerializeField] private float maxFear = 0f;
+    [SerializeField] private float fearDecreaseModifier = 0;
+    [SerializeField] private float fearIncreaseModifier = 0;
+
+    private float fearAmount = 0f;
+    private int nearEnemys = 0;
+
+    public UnityEvent OnFrightened;
+
+    private void Start()
+    {
+        fearAmount = 0f;
+    }
+
+    private void Update()
+    {
+        if (nearEnemys > 0)
+        {
+            fearAmount += nearEnemys * fearIncreaseModifier * Time.deltaTime;
+        }
+        else if (nearEnemys == 0 && fearAmount > 0)
+        {
+            fearAmount -= fearDecreaseModifier * Time.deltaTime;
+        }
+        else
+        {
+            fearAmount = 0f;
+        }
+
+        if (fearAmount >= maxFear)
+        {
+            OnFrightened.Invoke();
+        }
+    }
+
+    public void AddNearEnemy()
+    {
+        nearEnemys++;
+    }
+
+    public void RemoveNearEnemy()
+    {
+        nearEnemys--;
+    }
+}
---
+++ GGJ-2021-GAME/Assets/_Scripts/Spooker.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Spooker : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var fearMeter = collision.GetComponent<FearMeter>();
+
+            fearMeter.AddNearEnemy();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var fearMeter = collision.GetComponent<FearMeter>();
+
+            fearMeter.RemoveNearEnemy();
+        }
+    }
+}
