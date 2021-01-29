373ff521320946720416aefd5d7df8b7802a574e
Alejo Gaitán <thy.gaitan@gmail.com>
2021-01-29 17:54:48 -0300

    Basic Menu

---
+++ GGJ-2021-GAME/Assets/_Scripts/MenuController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickPlay() { SceneManager.LoadScene("Game"); }
+    public void OnClickCredits() { }
+    public void OnClickHelp() { }
+    public void OnClickBack() { }
+    public void OnClickExit() {
+        #if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+        #endif
+        Application.Quit();
+    }
+}
