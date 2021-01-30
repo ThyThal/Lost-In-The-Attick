c88a3a7fc738041f9e2d0138f5d657207d84f241
FacuHuergo <FacuHuergo@users.noreply.github.com>
2021-01-30 16:48:16 -0300

    + Barra de Miedo

--- GGJ-2021-GAME/Assets/_Scripts/Player/FearMeter.cs
+++ GGJ-2021-GAME/Assets/_Scripts/Player/FearMeter.cs
@@ -1,14 +1,17 @@
 ﻿using UnityEngine;
 using UnityEngine.Events;
-
+using UnityEngine.UI;
 public class FearMeter : MonoBehaviour
 {
-    [SerializeField] private float maxFear = 0f;
+    [SerializeField] private float maxFear = 10f;
     [SerializeField] private float fearDecreaseModifier = 0;
     [SerializeField] private float fearIncreaseModifier = 0;
+    private float _minimun;
+    [SerializeField] private Image _fearMask;
+    [SerializeField] private Color _color;
 
-    private float fearAmount = 0f;
-    private int nearEnemys = 0;
+    [SerializeField]private float fearAmount = 0f;
+    [SerializeField]private int nearEnemys = 0;
 
     public UnityEvent OnFrightened;
 
@@ -19,15 +22,20 @@ public class FearMeter : MonoBehaviour
 
     private void Update()
     {
+
+        //TODO: Incrementar el multiplicador de miedo por tiempo
+
         if (!GameManager.Instance.IsPaused())
         {
             if (nearEnemys > 0)
             {
                 fearAmount += nearEnemys * fearIncreaseModifier * Time.deltaTime;
+                GetCurrentFill();
             }
             else if (nearEnemys == 0 && fearAmount > 0)
             {
                 fearAmount -= fearDecreaseModifier * Time.deltaTime;
+                GetCurrentFill();
             }
             else
             {
@@ -49,5 +57,13 @@ public class FearMeter : MonoBehaviour
     public void RemoveNearEnemy()
     {
         nearEnemys--;
+    }
+    private void GetCurrentFill()
+    {
+        float currentOffset = fearAmount - _minimun;
+        float maximumOffset = maxFear - _minimun;
+        float fillAmount = currentOffset / maximumOffset;
+        _fearMask.fillAmount = fillAmount;
+        _fearMask.color = _color;
     }
 }
