cc2dc0b32dd4973dfd488adf07c67caff96583fe
Franco Rivas <francorivaa19@gmail.com>
2021-01-30 19:15:07 -0300

    sonido de pickUp!

--- GGJ-2021-GAME/Assets/_Scripts/ObjectQuestPickedUp.cs
+++ GGJ-2021-GAME/Assets/_Scripts/ObjectQuestPickedUp.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class ObjectQuestPickedUp : MonoBehaviour
 {
+    [SerializeField] 
+    private GameObject pickUpSound = null;
 
+    private void Start()
+    {
+        pickUpSound.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            pickUpSound.gameObject.SetActive(true);
+            this.gameObject.SetActive(false);
+        }
+    }
 }
