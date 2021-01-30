ea3b7ed97b26f5cb952327c3d433a6c5066d3317
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-01-30 09:56:11 -0300

    Decrease Battery

---
+++ GGJ-2021-GAME/Assets/_Scripts/Player/FlashlightBattery.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class FlashlightBattery : MonoBehaviour
+{
+    [SerializeField] private float maxBattery = 0f;
+    [SerializeField] private float decreaseBatteryModifier = 1f;
+
+    private float currentBattery = 0f;
+
+    private void Start()
+    {
+        InitializeBattery();
+    }
+
+    private void Update()
+    {
+        if (currentBattery > 0)
+        {
+            currentBattery += Time.deltaTime * decreaseBatteryModifier;
+        }
+    }
+
+    private void InitializeBattery()
+    {
+        currentBattery = maxBattery;
+    }
+}
