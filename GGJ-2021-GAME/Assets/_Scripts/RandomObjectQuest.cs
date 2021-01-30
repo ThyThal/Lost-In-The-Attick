bb0dc58e668107ec09a0988a61491fcf3d317477
Diego Gandolfo <Diego.Gandolfo@Gmail.com>
2021-01-30 15:26:05 -0300

    RandomObjectQuest Script Creation

---
+++ GGJ-2021-GAME/Assets/_Scripts/RandomObjectQuest.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObjectQuest : MonoBehaviour
+{
+    // Que seleccione un Objeto Random de una lista
+    // Que lo instancie usando el SpawnObject
+    // Que le diga al GameManager que Objeto es Condición de Victoria
+}
